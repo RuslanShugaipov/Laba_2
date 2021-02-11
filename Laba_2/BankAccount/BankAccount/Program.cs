f57d87b603a240279f641c75c2303bd2eea76722
RuslanShugaipov <58910717+RuslanShugaipov@users.noreply.github.com>
2021-02-11 20:45:20 +0300

    Add files via upload

---
+++ Laba_2/BankAccount/BankAccount/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount = AccountType.Checking;
+            AccountType platinumAccount = AccountType.Deposit;
+            Console.WriteLine(goldAccount);
+            Console.WriteLine(platinumAccount);
+        }
+    }
+}
---
+++ Laba_2/StructType/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructType
+{
+    public enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+            goldAccount.accNo = 11354782;
+            goldAccount.accBal = 100199;
+            goldAccount.accType = AccountType.Checking;
+            Console.WriteLine(goldAccount.accNo);
+            Console.WriteLine(goldAccount.accBal);
+            Console.WriteLine(goldAccount.accType);
+        }
+    }
+}
